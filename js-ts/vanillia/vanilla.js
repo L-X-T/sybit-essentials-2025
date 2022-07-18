c4cfb0dfd69f976feb52534c1fa3f92b4326461e
Alex Thalhammer <alex@angular-software.at>
2022-07-18 13:36:40 +0200

    [js-ts] functions

--- js-ts/vanillia/vanilla.js
+++ js-ts/vanillia/vanilla.js
@@ -13,3 +13,28 @@ console.debug('Verbose debug stuff');
 // console.log(window.navigator);
 
 // Start coding here
+function forEach(ary, action) {
+  for (var i = 0; i < ary.length; i++) {
+    action(ary[i]);
+  }
+}
+
+function showAlert(item) {
+  alert(item);
+}
+
+const myIntegers = [1, 2, 3];
+
+
+// <Main>
+
+// classic
+forEach(myIntegers, showAlert);
+
+// pass a function as parameter
+/*forEach(myIntegers, function (item) {
+  alert(item);
+});*/
+
+// pass a function as lambda
+// forEach(myIntegers, (item) => alert(item));
