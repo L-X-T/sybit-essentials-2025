ef82f95e512b59ef63388fd8dbb9fcbf95770b44
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2025-04-01 15:39:31 +0200

    convert AirportsComponent to use a signal

--- src/app/airports/airports.component.ts
+++ src/app/airports/airports.component.ts
@@ -1,13 +1,19 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { delay } from 'rxjs';
 import { AirportService } from './airport.service';
-import { AsyncPipe } from '@angular/common';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-airports',
   templateUrl: './airports.component.html',
-  imports: [AsyncPipe],
 })
 export class AirportsComponent {
-  protected readonly airports$ = inject(AirportService).findAll().pipe(delay(1_000));
+  airports = signal<string[]>([]);
+
+  constructor() {
+    inject(AirportService)
+      .findAll()
+      .pipe(delay(1_000), takeUntilDestroyed())
+      .subscribe((airports) => this.airports.set(airports));
+  }
 }
